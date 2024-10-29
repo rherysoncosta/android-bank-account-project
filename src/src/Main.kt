cce0042feffaf796c8b4f95eb25b69cee1fbd7f0
rherysoncosta <rherysonlima@gmail.com>
2024-10-29 17:20:55 +0000

    Starting the banking system interface

---
+++ src/src/Main.kt
@@ -0,0 +1,8 @@
+fun main () {
+
+    println("Welcome to your banking system.")
+    println("What type of account would you like to create?")
+    println("1. Debit account")
+    println("2. Credit account")
+    println("3. Checking account")
+}
