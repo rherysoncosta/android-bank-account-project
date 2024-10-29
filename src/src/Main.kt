9bb2ff0f42b2b9eb26ed4521767894e64d10c444
rherysoncosta <rherysonlima@gmail.com>
2024-10-29 20:25:50 +0000

    Setting the account type based on user input, and displaying the account type

--- src/src/Main.kt
+++ src/src/Main.kt
@@ -1,15 +1,40 @@
 fun main () {
 
-    var accountType = ""
-    var userChoice = 0
-
     println("Welcome to your banking system.")
+
     println("What type of account would you like to create?")
     println("1. Debit account")
     println("2. Credit account")
     println("3. Checking account")
-    println("Choose an option (1, 2 or 3:")
 
 
+    var accountType = ""
 
+
+    while (accountType == "") {
+            println("Choose an option (1, 2 or 3:")
+
+        val input = readlnOrNull()
+        val userChoice = input?.toIntOrNull() ?: 0
+
+        println("The selected option is... $userChoice")
+
+
+        when(userChoice) {
+            1 -> {
+                accountType = "debit"
+            }
+            2 -> {
+                accountType = "credit"
+            }
+            3 -> {
+                accountType = "checking"
+            }
+            else -> {
+                println("Invalid choice, please select again.")
+            }
+        }
+
+    }
+    println("Account created: $accountType")
 }
