2bf0f3e648b1b5270a8077b6786ecace065d46a6
rherysoncosta <rherysonlima@gmail.com>
2024-10-29 19:43:13 +0000

    Creating variables for the bank account type and user input

--- src/src/Main.kt
+++ src/src/Main.kt
@@ -1,9 +1,15 @@
 fun main () {
 
+    var accountType = ""
+    var userChoice = 0
+
     println("Welcome to your banking system.")
     println("What type of account would you like to create?")
     println("1. Debit account")
     println("2. Credit account")
     println("3. Checking account")
-    println("Choose an option (1, 2 or 3")
+    println("Choose an option (1, 2 or 3:")
+
+
+
 }
